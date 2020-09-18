7d2152c76d4746ca5ddb2ea9e18f46dc9b0c2e8d
FawzyGaffar <mohamedfawzy4u@gmail.com>
2020-09-18 10:46:34 +0530

    Update CscExample.java

--- CscExample.java
+++ CscExample.java
@@ -2,4 +2,16 @@ class CscExamples{
 	int questionNo;
 	String category;
 	String text;
+	
+	public setQuestionNo(int questionNo){
+		this.questionNo = questionNo;
+	}
+	
+	public setCategory(string category){
+		this.category = category;
+	}
+	
+	public setText(string text){
+		this.text =text;
+	}
 }
