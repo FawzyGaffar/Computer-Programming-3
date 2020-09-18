10f348a484f4f5d93a6e54bcb8204d1941a6d289
FawzyGaffar <mohamedfawzy4u@gmail.com>
2020-09-18 10:51:48 +0530

    Csc Example

--- CscExample.java
+++ CscExample.java
@@ -2,4 +2,16 @@ class CscExamples{
 	int questionNo;
 	String category;
 	String text;
+	
+	public addQuestion(CscExample qu){
+		
+	}
+	
+	public removeQuestion(int quID){
+		
+	}
+	
+	public editQuestion(int quID){
+		
+	}
 }
